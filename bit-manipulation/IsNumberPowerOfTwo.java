4361d80d32e048c1df49ad6045be1412531628d1
Oreste Luci <oreste@luci.cl>
2015-08-22 18:02:32 +0200

    Added IsNumberPowerOfTwo

---
+++ bit-manipulation/IsNumberPowerOfTwo.java
@@ -0,0 +1,36 @@
+import java.io.Console;
+
+/**
+ * Determines if a number X is 2^n
+ *
+ * @author Oreste Luci
+ */
+public class IsNumberPowerOfTwo {
+
+    public static void main(String[] args) {
+
+        Console console = System.console();
+
+        System.out.println("Enter number: ");
+        int number = Integer.parseInt(console.readLine());
+
+        System.out.println("Number is power of 2: " + isPowerOfTwo(number));
+    }
+
+    /**
+     * Thie method uses the following property;
+     * 2^n & (2^n - 1) = 0
+     */
+    public static boolean isPowerOfTwo(int number) {
+
+        int oneLess = number - 1;
+
+        int result = number & oneLess;
+
+        if (result == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
