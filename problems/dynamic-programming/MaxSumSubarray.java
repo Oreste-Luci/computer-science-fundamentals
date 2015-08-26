99c3511eb7ad09a210c90529a9d8fe49e172459e
Oreste Luci <oreste@luci.cl>
2015-08-26 22:58:15 +0200

    Added Diif method

--- problems/dynamic-programming/MaxSumSubarray.java
+++ problems/dynamic-programming/MaxSumSubarray.java
@@ -10,21 +10,27 @@ public class MaxSumSubarray {
         int[] array = {-5, 0, -4, 1, 4, 3, -2, -1, 2, -3};
         printArray(array);
         System.out.println("Max continuous sum: " + maxSubArraySum(array));
+        array = new int[] {-5, 0, -4, 1, 4, 3, -2, -1, 2, -3};
+        System.out.println("Max difference: " + maxDifference(array));
 
         System.out.println(" ");
         int[] array2 = {5, 10000, -1, 30000};
         printArray(array2);
         System.out.println("Max continuous sum: " + maxSubArraySum(array2));
+        array2 = new int[]{5, 10000, -1, 30000};
+        System.out.println("Max difference: " + maxDifference(array2));
 
         System.out.println(" ");
         int[] array3 = {5, 10000, -1, 30000, -40000};
         printArray(array3);
         System.out.println("Max continuous sum: " + maxSubArraySum(array3));
+        array3 =  new int[]{5, 10000, -1, 30000, -40000};
+        System.out.println("Max difference: " + maxDifference(array3));
+
+        System.out.println(" ");
     }
 
     public static int maxSubArraySum(int[] array) {
-
-        array[0] = Math.max(0,array[0]);
 
         // Adding continuous values
         for (int i=1; i<array.length; i++) {
@@ -40,6 +46,24 @@ public class MaxSumSubarray {
         }
 
         return max;
+    }
+
+    public static int maxDifference(int[] array) {
+
+        for (int i=1; i<array.length; i++) {
+            array[i] = array[i] - array[i-1];
+        }
+
+        System.out.println("Diff array:");
+        printArray(array);
+
+        // Finding the highest value
+        int max_diff = 0;
+        for (int i=0; i<array.length; i++) {
+            max_diff = Math.max(max_diff, array[i]);
+        }
+
+        return max_diff;
     }
 
     public static void printArray(int[] array) {
